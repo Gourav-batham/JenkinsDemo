f3779d829a879f264656d85f1c3fbbd396f5c8de
Gourav-batham <142788076+Gourav-batham@users.noreply.github.com>
2024-01-05 12:06:12 +0530

    Initial Commit

---
+++ src/test/java/jenkinslearning/JenkinsLearingApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkinslearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsLearingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
